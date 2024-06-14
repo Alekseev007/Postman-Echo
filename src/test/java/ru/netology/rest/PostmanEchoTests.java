f77c7b66ccb52a88dfeacd55014477aa469524c3
Vasiliy Alekseev <alekseev.v007@yandex.ru>
2024-06-14 12:08:16 +0300

    Test

--- src/test/java/ru/netology/rest/PostmanEchoTests.java
+++ src/test/java/ru/netology/rest/PostmanEchoTests.java
@@ -13,14 +13,14 @@ public class PostmanEchoTests {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Good") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Bad"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
